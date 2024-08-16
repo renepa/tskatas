38dafa6510d49224809cbda4878b10f6483d596c
René P <renepa@gmail.com>
2024-08-16 16:32:43 +0200

    Added tests and impl to sing between verses

--- src/99-bottles/my-solutions/99bootles.test.ts
+++ src/99-bottles/my-solutions/99bootles.test.ts
@@ -30,13 +30,27 @@ describe('BeerSong with dedicated verses', () => {
   })
 })
 
-describe('The full beerSong', () => {
+describe('Sing beer song between verse x and y', () => {
   const beerSong = new BeerSong()
 
-  it('sings the 8. verse only', () => {
-    const verse = beerSong.sing(8)
-    expect(verse).toEqual(
-      '8 bottles of beer on the wall, 8 bottles of beer. \\n Take one down and pass it around, 7 bottles of beer on the wall.'
+  it('sings from the 8. to the 5.', () => {
+    const song = beerSong.singFromTo(8, 6)
+    expect(song).toEqual(
+      '8 bottles of beer on the wall, 8 bottles of beer. \\n Take one down and pass it around, 7 bottles of beer on the wall. 7 bottles of beer on the wall, 7 bottles of beer. \\n Take one down and pass it around, 6 bottles of beer on the wall. 6 bottles of beer on the wall, 6 bottles of beer. \\n Take one down and pass it around, 5 bottles of beer on the wall. '
+    )
+  })
+
+  it('sings from the 3. to the 0.', () => {
+    const song = beerSong.singFromTo(2, 0)
+    expect(song).toEqual(
+      '2 bottles of beer on the wall, 2 bottles of beer. \\n Take one down and pass it around, 1 bottle of beer on the wall. 1 bottle of beer on the wall, 1 bottle of beer. \\n Take it down and pass it around, no more bottles of beer on the wall. No more bottles of beer on the wall, no more bottles of beer. \\n Go to the store and buy some more, 99 bottles of beer on the wall. '
+    )
+  })
+
+  it('sings 6. only', () => {
+    const song = beerSong.singFromTo(6, 6)
+    expect(song).toEqual(
+      '6 bottles of beer on the wall, 6 bottles of beer. \\n Take one down and pass it around, 5 bottles of beer on the wall. '
     )
   })
 })
--- src/99-bottles/my-solutions/BeerSongClass.ts
+++ src/99-bottles/my-solutions/BeerSongClass.ts
@@ -1,14 +1,24 @@
 export class BeerSong {
   sing(verseNumber: number): string {
-    if (verseNumber > 1) {
+    if (verseNumber > 2) {
       return `${verseNumber} bottles of beer on the wall, ${verseNumber} bottles of beer. \\n Take one down and pass it around, ${
         verseNumber - 1
       } bottles of beer on the wall.`
+    } else if (verseNumber === 2) {
+      return `2 bottles of beer on the wall, 2 bottles of beer. \\n Take one down and pass it around, 1 bottle of beer on the wall.`
     } else if (verseNumber === 1) {
       return `1 bottle of beer on the wall, 1 bottle of beer. \\n Take it down and pass it around, no more bottles of beer on the wall.`
     } else if (verseNumber === 0) {
       return 'No more bottles of beer on the wall, no more bottles of beer. \\n Go to the store and buy some more, 99 bottles of beer on the wall.'
     }
     throw Error('There is no verse < 0')
+  }
+
+  singFromTo(startVerseNumber: number, endVerseNumber: number): string {
+    let song = ''
+    for (let i = startVerseNumber; i >= endVerseNumber; i--) {
+      song += `${this.sing(i)} `
+    }
+    return song
   }
 }
