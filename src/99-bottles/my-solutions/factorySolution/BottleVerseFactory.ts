7f88084f6195a3e1a59621ab612f81744cbc4dd4
René P <renepa@gmail.com>
2024-10-24 23:33:16 +0200

    Error when number of bottles < 0

--- src/99-bottles/my-solutions/factorySolution/BottleVerseFactory.ts
+++ src/99-bottles/my-solutions/factorySolution/BottleVerseFactory.ts
@@ -4,6 +4,7 @@ import { NumberOfBottlesVerse0 } from './NumberOfBottlesVerse0'
 
 export class BottleVerseFactory {
   static forVerse(numberOfBottles: number): BottleVerse {
+    if (numberOfBottles < 0) throw new Error('Invilad number of bottles. Must not smaller than 0')
     if (numberOfBottles === 0) {
       return new NumberOfBottlesVerse0(numberOfBottles)
     }
