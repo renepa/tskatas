1c65a2785316ac00e50d63f8b32e2ffde741d741
René P <renepa@gmail.com>
2024-10-26 14:31:28 +0200

    switch in

--- src/99-bottles/my-solutions/factorySolution/BottleSongFactory/BottleVerseFactory.ts
+++ src/99-bottles/my-solutions/factorySolution/BottleSongFactory/BottleVerseFactory.ts
@@ -6,8 +6,14 @@ import { NumberOfBottlesVerse1 } from './NumberOfBottlesVerse1'
 export class BottleVerseFactory {
   static forVerse(numberOfBottles: number): BottleVerse {
     if (numberOfBottles < 0) throw new Error('Invilad number of bottles. Must not smaller than 0')
-    if (numberOfBottles === 0) return new NumberOfBottlesVerse0(numberOfBottles)
-    else if (numberOfBottles === 1) return new NumberOfBottlesVerse1(numberOfBottles)
-    return new NumberOfBottlesVerse(numberOfBottles)
+
+    switch (numberOfBottles) {
+      case 0:
+        return new NumberOfBottlesVerse0(numberOfBottles)
+      case 1:
+        return new NumberOfBottlesVerse1(numberOfBottles)
+      default:
+        return new NumberOfBottlesVerse(numberOfBottles)
+    }
   }
 }
